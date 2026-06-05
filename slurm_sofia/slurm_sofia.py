ada808751e0e2c76f1eb6ed2c6c656e4bf09d8cc
Samuel Moors <samuel.moors@vub.be>
2026-06-05 10:55:05 +0200

    update

--- slurm_sofia/slurm_sofia.py
+++ slurm_sofia/slurm_sofia.py
@@ -27,7 +27,7 @@ scontrol show job $SLURM_JOB_ID --json | jq -r '
 """
 
 TEMPJOB = r"""
-jobid=$(sbatch --parsable --time=5:0 --job-name={job_name} --wrap=hostname --clusters={cluster} \
+jobid=$(sbatch --parsable --time=5:0 --job-name={job_name} --wrap=hostname \
     --partition={slurm_partition} {extra})
 exitcode=$?
 if [[ $exitcode -ne 0 ]]; then exit $exitcode; fi
@@ -79,7 +79,7 @@ class SbatchDefaultCPUPerGPU(SlurmSofiaBase):
         return sn.all([
             sn.assert_found(rf'^cpus = {cpus}$', self.stdout, self.descr),
             sn.assert_found(rf'^gpus = {self.gpus}$', self.stdout, self.descr),
-            sn.assert_not_found(".", self.stderr, 'there should be no error messages')
+            sn.assert_not_found(".", self.stderr, 'there should be no error messages'),
         ])
 
 
@@ -88,6 +88,7 @@ class SlurmSofiaBaseLocal(SlurmSofiaBase):
     Base class for tests with Slurm jobs submitted locally
     """
     tags.add('local')
+    extra_job_opts = "--hold"
 
 
 @rfm.simple_test
@@ -97,17 +98,16 @@ class SbatchNoGPUs(SlurmSofiaBaseLocal):
     @run_after('setup')
     def set_executable(self):
         self.executable = TEMPJOB.format(
-            cluster=self.system,
             slurm_partition=self.gpu_partition,
             job_name=self.__class__.__name__,
-            extra="")
+            extra=self.extra_job_opts)
 
     @sanity_function
     def assert_allocation(self):
         error_msg = 'Only GPU jobs are allowed on this partition'
         return sn.all([
+            sn.assert_found(error_msg, self.stderr, f'error message should contain: "{error_msg}"'),
             sn.assert_eq(self.job.exitcode, 1, f'exit code should be 1, got {self.job.exitcode}'),
-            sn.assert_found(error_msg, self.stderr, f'error message should contain: {error_msg}')
         ])
 
 
@@ -123,17 +123,16 @@ class SbatchForbiddenCPUOptions(SlurmSofiaBaseLocal):
     @run_after('setup')
     def set_executable(self):
         self.executable = TEMPJOB.format(
-            cluster=self.system,
             slurm_partition=self.gpu_partition,
             job_name=self.__class__.__name__,
-            extra=f"--gpus-per-node=3 {self.job_opts}")
+            extra=f"{self.extra_job_opts} --gpus-per-node=3 {self.job_opts}")
 
     @sanity_function
     def assert_allocation(self):
         error_msg = f'Job option {self.job_opts.split("=")[0]} is not allowed on this partition'
         return sn.all([
+            sn.assert_found(error_msg, self.stderr, f'error message should contain: "{error_msg}"'),
             sn.assert_eq(self.job.exitcode, 1, f'exit code should be 1, got {self.job.exitcode}'),
-            sn.assert_found(error_msg, self.stderr, f'error message should contain: {error_msg}')
         ])
 
 
@@ -154,17 +153,16 @@ class SbatchCorrectCPUsPerGPU(SlurmSofiaBaseLocal):
         job_opts, gpus, modifier = self.job_opts
         job_opts = job_opts.format(cpus=modifier(self.default_cpus_per_gpu, gpus), gpus=gpus)
         self.executable = TEMPJOB.format(
-            cluster=self.system,
             slurm_partition=self.gpu_partition,
             job_name=self.__class__.__name__,
-            extra=f"--hold {job_opts}")
+            extra=f"{self.extra_job_opts} {job_opts}")
 
     @sanity_function
     def assert_allocation(self):
         msg = r'job submitted: \d+$'
         return sn.all([
+            sn.assert_found(msg, self.stdout, f'standard output should contain: "{msg}"'),
             sn.assert_eq(self.job.exitcode, 0, f'exit code should be 0, got {self.job.exitcode}'),
-            sn.assert_found(msg, self.stdout, f'error message should contain: {msg}')
         ])
 
 
@@ -184,8 +182,8 @@ class SbatchWrongCPUsPerGPU(SbatchCorrectCPUsPerGPU):
     def assert_allocation(self):
         error_msg = f'This partition requires exactly {self.default_cpus_per_gpu} CPUs per GPU'
         return sn.all([
+            sn.assert_found(error_msg, self.stderr, f'error message should contain: "{error_msg}"'),
             sn.assert_eq(self.job.exitcode, 1, f'exit code should be 1, got {self.job.exitcode}'),
-            sn.assert_found(error_msg, self.stderr, f'error message should contain: {error_msg}')
         ])
 
 
@@ -198,24 +196,23 @@ class SbatchForbiddenCombination(SlurmSofiaBaseLocal):
         f'--cpus-per-task=2 --nodes=2 --gpus-per-node={gpus}',
         f'--ntasks=2 --nodes=2 --gpus-per-node={gpus}',
         f'--ntasks=3 --ntasks-per-node=2 --gpus-per-node={gpus}',
-        f'--gpus-per-task=1 --cpus-per-task=2 --gpus-per-node={gpus}',
+        f'--gpus-per-task=1 --ntasks=2 --cpus-per-task=2 --gpus-per-node={gpus}',
         f'--gpus={gpus}',
     ])
 
     @run_after('setup')
     def set_executable(self):
         self.executable = TEMPJOB.format(
-            cluster=self.system,
             slurm_partition=self.gpu_partition,
             job_name=self.__class__.__name__,
-            extra=f"--hold {self.job_opts}")
+            extra=f"{self.extra_job_opts} {self.job_opts}")
 
     @sanity_function
     def assert_allocation(self):
         error_msg = 'The requested resource combination is not allowed on this partition'
         return sn.all([
+            sn.assert_found(error_msg, self.stderr, f'error message should contain: "{error_msg}"'),
             sn.assert_eq(self.job.exitcode, 1, f'exit code should be 1, got {self.job.exitcode}'),
-            sn.assert_found(error_msg, self.stderr, f'error message should contain: {error_msg}')
         ])
 
 
@@ -225,22 +222,26 @@ class SbatchForbiddenMemOptions(SlurmSofiaBaseLocal):
     job_opts = parameter([
         '--mem=1000',
         '--mem-per-cpu=1000',
-        '--mem-per-gpu=1000',
+        '--mem-per-gpu=1000 --gpus-per-node=1',
     ])
 
     @run_after('setup')
     def set_executable(self):
+        if self.job_opts.startswith('--mem-per-gpu'):
+            slurm_partition = self.gpu_partition
+        else:
+            slurm_partition = self.cpu_partition
+
         self.executable = TEMPJOB.format(
-            cluster=self.system,
-            slurm_partition=self.cpu_partition,
+            slurm_partition=slurm_partition,
             job_name=self.__class__.__name__,
-            extra=self.job_opts)
+            extra=f"{self.extra_job_opts} {self.job_opts}")
 
     @sanity_function
     def assert_allocation(self):
         error_msg = (f'Job submit option {self.job_opts.split("=")[0]} is not allowed: memory is assigned '
                      'automatically per CPU core')
         return sn.all([
+            sn.assert_found(error_msg, self.stderr, f'error message should contain: "{error_msg}"'),
             sn.assert_eq(self.job.exitcode, 1, f'exit code should be 1, got {self.job.exitcode}'),
-            sn.assert_found(error_msg, self.stderr, f'error message should contain: {error_msg}')
         ])
